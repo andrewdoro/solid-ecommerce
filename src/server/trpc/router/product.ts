00666028e63333af0960fd40023af2226bef4011
Andrei Dorobantu <elfandreis@gmail.com>
2023-01-15 23:52:22 +0200

    fix: prisma init

--- src/server/trpc/router/product.ts
+++ src/server/trpc/router/product.ts
@@ -1,8 +1,9 @@
 import { procedure, router } from "../utils";
+import { prisma } from "../../db/client";
 
 export const productRouter = router({
   all: procedure.query(async () => {
-    const products = prisma?.product.findMany();
+    const products = prisma.product.findMany();
     return products;
   }),
 });
